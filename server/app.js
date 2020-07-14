47d3e96baf9a266a90fb85fea4c568b751d05776
ravinalawade <ravinalawade5@gmail.com>
2020-07-14 15:11:04 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -19,6 +19,20 @@ var app = express();
 app.set('view engine', 'ejs');
 
 // middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain)
 app.use(bodyParser.json());
 app.use(cors());
 // app.use(passport.initialize());
